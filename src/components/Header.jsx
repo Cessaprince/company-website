bacbdd422347c9fde3ec5f3ce4bffd8beac82785
Cessaprince <cessaprince2007@gmail.com>
2026-07-09 10:33:56 +0100

    designed the mobile menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,17 +8,17 @@ const Header = () => {
     //check if the link is active using the props.isActive but destructured
     const navLinkClass = ({ isActive }) =>
         `py-[7px] px-[13px] rounded-[35px] text-[14px] font-light transition-all duration-300 
-    ${isActive ? "bg-[var(--dark-purple)] text-white" : "text-gray-300 hover:text-white" }`;
+    ${isActive ? "bg-[var(--dark-purple)] text-white" : "text-gray-300 hover:text-white"}`;
 
     return (
         <div>
-            <section className="w-full bg-black h-auto py-[12px] max-md:hidden" id='myHeader'>
+            <section className="header w-full bg-black h-auto py-[12px]" id='myHeader'>
                 <div className="short-section flex items-center justify-between gap-[30px]">
                     <div className="h-[30px] w-[30px] overflow-hidden">
                         <img src={logo} alt="" className="w-full h-full object-cover" />
                     </div>
 
-                    <nav className="py-2 px-2 flex items-center gap-[12px] border-gray-800  bg-[var(--dark-blue2)] border-[1px] rounded-[35px] capitalize w-auto">
+                    <nav className="py-2 px-2 flex items-center gap-[12px] border-gray-800  bg-[var(--dark-blue2)] border-[1px] rounded-[35px] capitalize w-auto max-md:hidden">
                         <NavLink to="/" className={navLinkClass}>
                             home
                         </NavLink>
@@ -36,10 +36,26 @@ const Header = () => {
                         </NavLink>
                     </nav>
 
-                    <Link to='/contact-us' className="group text-[14px] text-gray-200 inline-flex items-center gap-2 bg-[var(--dark-purple)] py-[7px] px-[20px] rounded-[35px] hover:bg-[var(--light-purple2)] cursor-pointer">
+                    <Link to='/contact-us' className="group text-[14px] text-gray-200 inline-flex items-center gap-2 bg-[var(--dark-purple)] py-[7px] px-[20px] rounded-[35px] hover:bg-[var(--light-purple2)] cursor-pointer max-md:hidden">
                         <span className="group-hover:text-white">Book a Call</span>
                         <FontAwesomeIcon icon={['fas', 'arrow-right']} className="group-hover:text-white" size="xs" />
                     </Link>
+
+                    <div className="border-[0.1px] flex items-center justify-center w-[40px] h-[40px] border-gray-800 text-white rounded-[10px] cursor-pointer hidden max-md:flex ">
+                        <FontAwesomeIcon icon={['fas', 'bars']} className="text-white text-[18px]" />
+                    </div>
+                </div>
+
+                <div className="mobile-nav border-gray-600 border-[0.1px] p-[15px] m-[15px] rounded-[15px] flex flex-col gap-[2px] hidden max-md:flex">
+                    <Link to='/' className="capitalize  text-gray-300 py-[15px] px-[20px] w-full rounded-[15px] hover:bg-blue-700 hover:text-white">home</Link>
+                    <Link to='/about' className="capitalize  text-gray-300 py-[15px] px-[20px] w-full rounded-[15px] hover:bg-blue-700 hover:text-white">who we are</Link>
+                    <Link to='/services' className="capitalize  text-gray-300 py-[15px] px-[20px] w-full rounded-[15px] hover:bg-blue-700 hover:text-white">what we do</Link>
+                    <Link to='/products' className="capitalize  text-gray-300 py-[15px] px-[20px] w-full rounded-[15px] hover:bg-blue-700 hover:text-white">our products</Link>
+                    <Link to='/contact-us' className="group text-[14px] text-gray-300 flex items-center justify-center gap-2 bg-[var(--dark-purple)] py-[10px] px-[20px] rounded-[35px] hover:bg-[var(--light-purple2)] cursor-pointer">
+                        <span className="group-hover:text-white">Book a Call</span>
+                        <FontAwesomeIcon icon={['fas', 'arrow-right']} className="group-hover:text-white" size="xs" />
+                    </Link>
+
                 </div>
             </section>
         </div>
